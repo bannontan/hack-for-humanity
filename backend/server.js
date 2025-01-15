870f9429c075330bc6b2d06adc24cd67e41c16b7
bannontan <bannontan2001@hotmail.com>
2025-01-14 22:38:35 -0800

    Feature to create users database with a users table. users table required email, password and role.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import { createUsersDb } from "./utils/db/usersDb.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+// Users database
+createUsersDb();
 
 app.get("/", (req, res, next) => {
 	res.send("Hello World");
